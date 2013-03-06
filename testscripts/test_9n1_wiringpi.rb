b23ebcfb8f414440c2e468d2df489ec9b2cc0ae5
Christian Fischer <github@computerlyrik.de>
2013-03-06 20:38:18 +0000

    fixed test for 9n1

--- testscripts/test_9n1_wiringpi.rb
+++ testscripts/test_9n1_wiringpi.rb
@@ -12,11 +12,14 @@ $serial = WiringPi::Serial.new("/dev/ttyAMA0",9600)
 
 
 bytes  = [0,3,0x66,0x6f,0x6f,0,0x80]
+$serial.serialParity(0)
 while 1 do
   $io.write(0,HIGH)
 
-#  $serial.serialPut9char( 0b10101010,0)
-  $serial.serialPut9char( 0b10101011,1)
+  $serial.serialPut9char( 0b10100001,1)
+  $serial.serialPut9char( 0b00000000,0)
+
+#  $serial.serialPut9char( 0b01000010,1)
 #  $serial.serialPut9char(0b10100001,1)
 #  $serial.serialPut9char(0,0)
 #  $serial.serialPut9char(0,0)
@@ -28,6 +31,7 @@ while 1 do
  # $io.write(0,LOW)
  # rec_bytes(2,1)
  # rec_bytes(7,0)
+sleep 0.01
 end
 
 
