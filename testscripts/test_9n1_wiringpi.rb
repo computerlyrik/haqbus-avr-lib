0bddff53aafc2d86edfd7f55ec46360ebb36ea9f
Christian Fischer <github@computerlyrik.de>
2013-03-07 03:17:15 +0000

    package pingpong

--- testscripts/test_9n1_wiringpi.rb
+++ testscripts/test_9n1_wiringpi.rb
@@ -9,31 +9,33 @@ $io = WiringPi::GPIO.new
 $io.mode(0,OUTPUT)
 
 $serial = WiringPi::Serial.new("/dev/ttyAMA0",19200) 
+$serial.serialParity(0)
 
 def rec_bytes(num,parity)
-#  $sp.parity=parity
   num.times {
-    puts $serial.serialGetchar #.unpack('H*')
+    puts $serial.serialGetchar.to_s(16) #.unpack('H*')
   }
 end
 
 
-bytes  = [0,3,0x6,0x6f,0x6f,2,0x80]
-$serial.serialParity(0)
+bytes  = [0,4,0x06,0x6f,0x5,0x20,2,0x80]
 while 1 do
+sleep 0.1 
   $io.write(0,HIGH)
-
+sleep 0.1 
 
   $serial.serialPut9char(0,1)
   $serial.serialPut9char(0,1)
   bytes.each do |b|
  	$serial.serialPut9char(b,0)
   end
-sleep 0.010 
+sleep 0.1 
   $io.write(0,LOW)
+sleep 0.1 
   rec_bytes(2,1)
-  rec_bytes(7,0)
+  rec_bytes(8,0)
 sleep 0.01
+puts '----'
 end
 
 
