ff3673ec8b960b23101fb1540b560e13d339876f
Christian Fischer <github@computerlyrik.de>
2013-03-07 01:00:36 +0000

    working sent to raspberry

--- testscripts/test_9n1_wiringpi.rb
+++ testscripts/test_9n1_wiringpi.rb
@@ -8,25 +8,20 @@ require 'wiringpi'
 $io = WiringPi::GPIO.new
 $io.mode(0,OUTPUT)
 
-$serial = WiringPi::Serial.new("/dev/ttyAMA0",9600) 
+$serial = WiringPi::Serial.new("/dev/ttyAMA0",19200) 
 
 
-bytes  = [0,3,0x66,0x6f,0x6f,0,0x80]
+bytes  = [0,3,0x6,0x6f,0x6f,0,0x80]
 $serial.serialParity(0)
 while 1 do
   $io.write(0,HIGH)
 
-  $serial.serialPut9char( 0b10100001,1)
-  $serial.serialPut9char( 0b00000000,0)
 
-#  $serial.serialPut9char( 0b01000010,1)
-#  $serial.serialPut9char(0b10100001,1)
-#  $serial.serialPut9char(0,0)
-#  $serial.serialPut9char(0,0)
-#  $serial.serialPut9char(0,1)
-#  bytes.each do |b|
-# 	$serial.serialPut9char(b,0)
-#  end
+  $serial.serialPut9char(0,1)
+  $serial.serialPut9char(0,1)
+  bytes.each do |b|
+ 	$serial.serialPut9char(b,0)
+  end
  
  # $io.write(0,LOW)
  # rec_bytes(2,1)
